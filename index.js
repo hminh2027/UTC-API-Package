e204736525a21226f069efaf365f0e115a7218e5
Minh <dackhers@gmail.com>
2022-02-04 01:24:32 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+
+export const getAllSchedule = async (username, password) => {
+    if (!username || !password) return {data: '', error: 'Username or password not found!'}
+    try {
+        const rs = await axios.post('http://utc-schedule-api.herokuapp.com/schedule', {
+            username,
+            password
+        })
+
+        return {data: rs.data.data, error:''}
+
+    } catch (error) {
+        return {data: '', error: error.response.data.error}
+    }
+}
+
+export const getScheduleOfToday = async (username, password) => {
+    if (!username || !password) return {data: '', error: 'Username or password not found!'}
+    try {
+        const rs = await axios.post('http://utc-schedule-api.herokuapp.com/schedule/today', {
+            username,
+            password
+        })
+
+        return {data: rs.data.data, error:''}
+
+    } catch (error) {
+        return {data: '', error: error.response.data.error}
+    }
+}
+
+export const getScheduleOfDay = async (username, password, date) => {
+    if (!username || !password) return {data: '', error: 'Username or password not found!'}
+    if (!date) return {data: '', error: 'Date not found!'}
+    try {
+        const rs = await axios.post(`http://utc-schedule-api.herokuapp.com/schedule/day?year=${date.getFullYear()}&month=${date.getMonth()}&day=${date.getDate()}`, {
+            username,
+            password
+        })
+
+        return {data: rs.data.data, error:''}
+
+    } catch (error) {
+        return {data: '', error: error.response.data.error}
+    }
+}
